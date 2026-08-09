b9e07d606e96ec7ffa57905f75ed80381f8eddc6
axy-dev-me <yazilli73@gmail.com>
2026-08-09 21:53:40 +0500

    added print_string function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,21 @@
+volatile unsigned short* video_memory = (volatile unsigned short*) 0xB8000;
+unsigned short cursor = 0;
+
+void print_string(char* text, int color) {
+    for (short i = 0; text[i] != '\0'; i++) {
+        video_memory[cursor] = text[i] | (color << 8);
+        cursor++;
+    }
+}
+
 __attribute__((section(".text.entry")))
 void kernel() {
-    volatile unsigned short* video_memory = (volatile unsigned short*) 0xB8000;
-
     unsigned short blank = 0x0720; 
     for (int i = 0; i < 4000; i++) {
         video_memory[i] = blank;
     }
 
-    video_memory[0] = 'H' | (0x0F << 8);
-    video_memory[1] = 'e' | (0x0F << 8);
-    video_memory[2] = 'l' | (0x0F << 8);
-    video_memory[3] = 'l' | (0x0F << 8);
-    video_memory[4] = 'o' | (0x0F << 8);
+    print_string("Hello, World!", 0x0f);
 
     while(1) { __asm__("hlt"); }
 }
