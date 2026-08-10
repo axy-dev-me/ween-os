6def95cad439e50a28e291ade7f336f836455a40
axy-dev-me <yazilli73@gmail.com>
2026-08-10 12:42:46 +0500

    move VGA functions to a separate vga.h driver

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ arch = i386
 run:
 	mkdir -p tmp
 	nasm -fbin boot/boot.s -o tmp/boot.bin
-	gcc -m32 -fno-pie -ffreestanding -Iinclude -nostdlib -c kernel/kernel.c -o tmp/kernel.o
+	gcc -m32 -fno-pie -ffreestanding -Iinclude -Idrivers -nostdlib -c kernel/kernel.c -o tmp/kernel.o
 	ld -m elf_i386 -T linker.ld --oformat binary tmp/kernel.o -o tmp/kernel.bin
 	cat tmp/boot.bin tmp/kernel.bin > tmp/os.bin
 	qemu-system-$(arch) tmp/os.bin
@@ -12,7 +12,7 @@ run:
 build:
 	mkdir -p tmp build
 	nasm -fbin boot/boot.s -o tmp/boot.bin
-	gcc -m32 -fno-pie -ffreestanding -Iinclude -nostdlib -c kernel/kernel.c -o tmp/kernel.o
+	gcc -m32 -fno-pie -ffreestanding -Iinclude -Idrivers -nostdlib -c kernel/kernel.c -o tmp/kernel.o
 	ld -m elf_i386 -T linker.ld --oformat binary tmp/kernel.o -o tmp/kernel.bin
 	cat tmp/boot.bin tmp/kernel.bin > build/os.bin
 	rm -rf tmp
---
+++ drivers/vga.h
@@ -0,0 +1,62 @@
+#ifndef VGA_H
+#define VGA_H
+
+static volatile unsigned short* video_memory = (volatile unsigned short*) 0xB8000;
+static unsigned short cursor = 0;
+
+static inline int int_len(int num) {
+    if (num == 0) return 1;
+    if (num < 0) num = -num;
+    short len = 0;
+    while (num > 0) {
+        len++;
+        num /= 10;
+    }
+    return len;
+}
+
+static inline void print_char(char chr, int color) {
+    if (chr == '\n') {
+        cursor = ((cursor / 80) + 1) * 80;
+    } else {
+        video_memory[cursor] = chr | (color << 8);
+        cursor++;
+    }
+}
+
+static inline void print_string(char* text, int color) {
+    for (short i = 0; text[i] != '\0'; i++) {
+        print_char(text[i], color);
+    }
+}
+
+static inline void print_int(int num, int color) {
+    if (num == 0) { 
+        print_char('0', color); 
+        return; 
+    }
+    if (num < 0) {
+        print_char('-', color);
+        num = -num;
+    }
+
+    int len = int_len(num);
+    int target_pos = cursor + len - 1;
+
+    while (num > 0) {
+        video_memory[target_pos] = ((num % 10) + '0') | (color << 8);
+        target_pos--;
+        num /= 10;
+    }
+
+    cursor += len;
+}
+
+
+static inline void clear_screen(void) {
+    for (int i = 0; i < 4000; i++) {
+        video_memory[i] = (unsigned short)0x0720;
+    }
+}
+
+#endif
