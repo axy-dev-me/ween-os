228f4fb46b7e7ae18143b008fc02c6d9415ac8ef
axy-dev-me <yazilli73@gmail.com>
2026-08-13 13:01:21 +0500

    add gdt, switch to pm and mini kernel in asm

--- boot/boot.s
+++ boot/boot.s
@@ -8,14 +8,9 @@ _start:
     xor ax, ax
     mov ds, ax
     mov es, ax
-    
-    mov cl, 0x02
-    mov bx, 0x8000
-    call load_disk
 
-    mov ah, 0x0e
-    mov al, [0x8000]
-    int 0x10
+    call load_disk
+    call switch_to_pm
 
     hlt
     jmp $
@@ -23,7 +18,17 @@ _start:
 boot_drive: db 0
 
 %include "boot/disk.s"
+%include "boot/gdt.s"
+%include "boot/switch.s"
 
 times 510-($-$$) db 0
 dw 0xaa55
-db 's'
+
+kernel:
+bits 32
+    mov byte [0xb8000], 'K'
+    mov byte [0xb8001], 0x0a
+    hlt
+    jmp $
+
+times 512-($-kernel) db 0
--- boot/disk.s
+++ boot/disk.s
@@ -1,18 +1,14 @@
+bits 16
 load_disk:
     pusha
     
-    mov ah, 0x02
-    mov al, [sectors_read]
-    mov ch, 0x00
-    mov dh, 0x00
+    mov ah, 0x42
     mov dl, [boot_drive]
+    mov si, dap
 
-    int 0x13
+    int 0x13 
 
     jc .error
-
-    cmp al, [sectors_read]
-    jne .error
 
     popa
     ret
@@ -21,7 +17,20 @@ load_disk:
     mov ah, 0x0e
     mov al, 'e'
     int 0x10
+    
     hlt
     jmp $
 
 sectors_read: db 1
+
+align 4
+dap:
+    db 0x10
+    db 0x00
+    dw 1                  ; sectors to read
+
+    ; bx and es
+    dw 0x0
+    dw 0x0800
+    
+    dq 1
